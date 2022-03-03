a88c87c276526e276936e3ca1c918ab5995c6e86
amir otd <amir.otd.1@gmail.com>
2022-03-03 22:31:30 +0100

    added comments

--- 2048/Engine.py
+++ 2048/Engine.py
@@ -13,12 +13,15 @@ class Game2048:
         self.game_over = False
 
     def print_board(self):
+        """
+         This method prints the whole game main board in tables
+        """
         largest = 0
-        for row in range(self.DIMENSION):
+        for row in range(self.DIMENSION):   # finds the largest number in the board
             for num in range(self.DIMENSION):
                 if self.board[row][num] > largest:
                     largest = self.board[row][num]
-        count = len(str(largest))
+        count = len(str(largest))   # every cell of the table will be equal to the size of the largest number
 
         print("=" * 20)
         for i in range(self.DIMENSION):
@@ -30,19 +33,28 @@ class Game2048:
                     print(' ' * (count-len(str(self.board[i][j])))+str(self.board[i][j])+'|', end='')
             print()
         print("=" * 20)
-    
+
     def generate_random(self):
-        ran_x = random.randrange(0, 4, 1)
-        ran_y = random.randrange(0, 4, 1)
-        if self.board[ran_x][ran_y] != 0:
+        """
+         This method generates two random numbers,
+        this two numbers are the coordinate of a cell
+        """
+        ran_x = random.randrange(0, self.DIMENSION, 1)
+        ran_y = random.randrange(0, self.DIMENSION, 1)
+        if self.board[ran_x][ran_y] != 0:   # return numbers if the cell is empty
             return self.generate_random()
         else:
             return ran_x, ran_y
 
     def main(self):
+        """
+        This method is the main loop of the game
+        """
         while not self.game_over:
             valid_input = True
             direction = input("Enter a direction(w,a,s,d) ").lower()
+
+            # copy the board in another list to check if the direction made is available or not
             temp_board = copy.deepcopy(self.board)
 
             if direction == 'w':

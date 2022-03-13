de33c495f250d15324b54dc9d6b170c961d48ea1
amir otd <amir.otd.1@gmail.com>
2022-03-13 15:44:32 +0100

    added load_images method

--- 2048/GameGUI.py
+++ 2048/GameGUI.py
@@ -8,10 +8,22 @@ class GameGUI:
         self.SCREEN_COORDINATES = (512, 512)
         self.DIMENSION = 4
         self.SQ_SIZE = self.SCREEN_COORDINATES[0] // self.DIMENSION
+        self.IMAGES = {}
 
         pygame.init()
 
+    def load_images(self):
+        """
+         This method loads images into pygame.
+        """
+        blocks = ['2', '4', '8', '16', '32', '64', '128', '256', '512', '1024', '2048']
+        for block in blocks:
+            self.IMAGES[block] = pygame.transform.scale(pygame.image.load("images/" + block + ".svg"),
+                                                        (self.SQ_SIZE, self.SQ_SIZE))
+
     def main(self):
+        self.load_images()
+
         while True:
             pygame.display.set_caption("2048 Game")
             main_board = pygame.display.set_mode(self.SCREEN_COORDINATES)

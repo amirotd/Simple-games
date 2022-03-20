4d454ac9a9e48da77e717986bf4cce0ba1109cc5
amir otd <amir.otd.1@gmail.com>
2022-03-20 20:21:58 +0100

    added Score box to GameGUI.py

--- 2048/GameGUI.py
+++ 2048/GameGUI.py
@@ -6,13 +6,15 @@ import copy
 
 class GameGUI:
     def __init__(self):
-        self.SCREEN_COORDINATES = (400, 400)
+        self.SCREEN_COORDINATES = (500, 600)
+        self.BOARD_COORDINATES = (self.SCREEN_COORDINATES[0]-100, self.SCREEN_COORDINATES[1]-200)
         self.DIMENSION = 4
-        self.SQ_SIZE = self.SCREEN_COORDINATES[0] // self.DIMENSION
+        self.SQ_SIZE = self.BOARD_COORDINATES[0] // self.DIMENSION
         self.SQ_BORDER = 3
         self.IMAGES = {}
 
         pygame.init()
+        self.font = pygame.font.SysFont('arial', 40)
 
     def load_images(self):
         """
@@ -23,9 +25,13 @@ class GameGUI:
             image_size = (self.SQ_SIZE-(self.SQ_BORDER*2), self.SQ_SIZE-(self.SQ_BORDER*2))
             self.IMAGES[block] = pygame.transform.scale(pygame.image.load("images/" + block + ".png"), image_size)
 
-    def refresh(self, surface, stat):
+    def refresh(self, screen, surface, stat):
+        pygame.draw.rect(screen, 'dark grey', [0, 0, self.SCREEN_COORDINATES[0], self.SCREEN_COORDINATES[1]])
+        screen.blit(surface, [50, 150, self.BOARD_COORDINATES[0], self.BOARD_COORDINATES[1]])
+
         self.load_board(surface)
         self.update_blocks(surface, stat)
+        self.update_score(screen, stat)
 
     def load_board(self, surface):
         for row in range(self.DIMENSION):
@@ -42,12 +48,21 @@ class GameGUI:
                     rect = [col * self.SQ_SIZE + self.SQ_BORDER, row * self.SQ_SIZE + self.SQ_BORDER, self.SQ_SIZE, self.SQ_SIZE]
                     surface.blit(self.IMAGES[str(block)], rect)
 
+    def update_score(self, screen, stat):
+        score_surface = self.font.render("Score: " + str(stat.score), True, "0x494949")
+        score_rect = score_surface.get_rect(topleft=(50, 80))
+        screen.blit(score_surface, score_rect)
+
     def main(self):
         self.load_images()
         status = Game2048()
         pygame.display.set_caption("2048 Game")
         pygame.display.set_icon(self.IMAGES['2048'])
-        main_board = pygame.display.set_mode(self.SCREEN_COORDINATES)
+
+        screen = pygame.display.set_mode(self.SCREEN_COORDINATES)
+        screen.fill(pygame.Color("dark grey"))
+
+        main_board = pygame.Surface(self.BOARD_COORDINATES)
         main_board.fill(pygame.Color("grey"))
 
         status.get_random_num()
@@ -86,7 +101,7 @@ class GameGUI:
                             if status.check_losing():
                                 print("you lost :(")
                                 break
-            self.refresh(main_board, status)
+            self.refresh(screen, main_board, status)
             pygame.display.update()
 
 

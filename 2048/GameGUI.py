c2822c5e0fa7f730b76910559a0390910c255fab
amir otd <amir.otd.1@gmail.com>
2022-03-26 14:16:05 +0100

    ending screen methods were replaced with one method

--- 2048/GameGUI.py
+++ 2048/GameGUI.py
@@ -34,9 +34,9 @@ class GameGUI:
         screen.blit(surface, [50, 150, self.BOARD_COORDINATES[0], self.BOARD_COORDINATES[1]])
 
         if stat.check_losing():
-            self.losing_message(screen)
+            self.ending_message(screen, "You Lost!", "red")
         elif stat.check_winning():
-            self.winning_message(screen)
+            self.ending_message(screen, "You Won!", "green")
         self.load_board(surface)
         self.update_blocks(surface, stat)
         self.update_score(screen, stat)
@@ -61,29 +61,17 @@ class GameGUI:
         score_rect = score_surface.get_rect(topleft=(50, 80))
         screen.blit(score_surface, score_rect)
 
-    def losing_message(self, screen):
+    def ending_message(self, screen, message: str, color: str):
         if self.end_game_screen_value != self.BOARD_COORDINATES[0]:
             self.end_game_screen_value += 10
-        losing_surface = pygame.Surface((self.end_game_screen_value, self.end_game_screen_value))
-        losing_surface.set_alpha(100)
-        losing_surface.fill("red")
+        ending_surface = pygame.Surface((self.end_game_screen_value, self.end_game_screen_value))
+        ending_surface.set_alpha(100)
+        ending_surface.fill(color)
 
-        losing_text = self.font2.render("You Lost!", True, "white")
-        losing_text_rect = losing_text.get_rect(center=(self.SCREEN_COORDINATES[0] // 2, self.SCREEN_COORDINATES[1] // 2))
-        screen.blit(losing_surface, (250 - self.end_game_screen_value // 2, 350 - self.end_game_screen_value // 2))
-        screen.blit(losing_text, losing_text_rect)
-
-    def winning_message(self, screen):
-        if self.end_game_screen_value != self.BOARD_COORDINATES[0]:
-            self.end_game_screen_value += 10
-        winning_surface = pygame.Surface((self.end_game_screen_value, self.end_game_screen_value))
-        winning_surface.set_alpha(100)
-        winning_surface.fill("green")
-
-        winning_text = self.font2.render("You Won!", True, "white")
-        winning_text_rect = winning_text.get_rect(center=(self.SCREEN_COORDINATES[0] // 2, self.SCREEN_COORDINATES[1] // 2))
-        screen.blit(winning_surface, (250 - self.end_game_screen_value // 2, 350 - self.end_game_screen_value // 2))
-        screen.blit(winning_text, winning_text_rect)
+        ending_text = self.font2.render(message, True, "white")
+        ending_text_rect = ending_text.get_rect(center=(self.SCREEN_COORDINATES[0] // 2, self.SCREEN_COORDINATES[1] // 2))
+        screen.blit(ending_surface, (250 - self.end_game_screen_value // 2, 350 - self.end_game_screen_value // 2))
+        screen.blit(ending_text, ending_text_rect)
 
     def main(self):
         self.load_images()

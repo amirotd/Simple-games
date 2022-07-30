a8eb1e4cb71b849cc346b2f40d10a6f2cfd1c4c9
amir otd <amir.otd.1@gmail.com>
2022-07-30 23:45:57 +0430

    [tet] added game board & main loop

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -1,5 +1,11 @@
 import os
+import sys
+import select
+import tty
+import termios
 import random
+import time
+from copy import deepcopy
 
 
 class Tetris:
@@ -50,15 +56,79 @@ class Tetris:
                       [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
                       [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]]
 
-    def clear_screen(self):
-        pass
+    @staticmethod
+    def clear_screen():
+        os.system('clear')
 
-    def print_board(self):
-        pass
+    def print_board(self, current_piece, piece_pos):
+        self.clear_screen()
+        board_copy = deepcopy(self.board)
+        current_piece_size_y = len(current_piece)
+        current_piece_size_x = len(current_piece)
+        for i in range(current_piece_size_y):
+            for j in range(current_piece_size_x):
+                board_copy[piece_pos[0]+i][piece_pos[1]+j] = current_piece[i][j] | self.board[piece_pos[0]+i][piece_pos[1]+j]
+
+        print("")
+        print(" " * 10 + "\033[1;32m =" * 12)
+        for i in range(22):
+            print(" " * 10, end='')
+            for j in range(12):
+                if board_copy[i][j] == 1:
+                    print("▌▌", end='')
+                elif board_copy[i][j] == 2:
+                    print("|+|", end='')
+                elif board_copy[i][j] == 3:
+                    print(" =", end='')
+                else:
+                    print("..", end='')
+
+            print("")
+
+    def get_piece(self):
+        return random.choice(self.pieces)
+
+    def get_down(self, piece_pos):
+        new_piece_pos = [piece_pos[0] + 1, piece_pos[1]]
+        return new_piece_pos
+
+    @staticmethod
+    def kbhit():
+        return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
     def main(self):
-        pass
+        current_piece = self.get_piece()
+        piece_pos = [0, 4]
+        old_settings = termios.tcgetattr(sys.stdin)
+        try:
+            tty.setcbreak(sys.stdin.fileno())
+            self.clear_screen()
+            input('press ENTER to start...')
+            while True:
+                if self.kbhit():
+                    c = sys.stdin.read(1)
+                    if c == 'w':
+                        pass
+                    if c == 'a':
+                        pass
+                    if c == 'd':
+                        pass
+                    if c == 's':
+                        pass
+                    if c == 'g':
+                        break
+
+                piece_pos = self.get_down(piece_pos)
+                self.print_board(current_piece, piece_pos)
+                time.sleep(0.3)
+
+        finally:
+            self.clear_screen()
+            print("game over")
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
 
 if __name__ == '__main__':
     print("Hello this is Tetris")
+    tetris = Tetris()
+    tetris.main()
